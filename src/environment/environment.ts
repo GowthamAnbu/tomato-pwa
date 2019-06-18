a35fc5557c41c4004adb7e1909dd176c804ce849
Gowtham Anbu <gautamgow@gmail.com>
2019-06-18 19:49:35 +0530

    removed firebase credentials

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,11 +2,7 @@ export const environment = {
   apiBaseUrl:'https://pwa.techaffinity.com/'
 };
 
+// tomato-567
 export const firebaseConfig = {
-  apiKey: "AIzaSyCe21tNrsMx30IXHsvgt-LQPHAJ0EHQdcI",
-  authDomain: "tomato-567.firebaseapp.com",
-  databaseURL: "https://tomato-567.firebaseio.com",
-  projectId: "tomato-567",
-  storageBucket: "tomato-567.appspot.com",
-  messagingSenderId: "48068424744"
+//   paste the credentials here
 };
